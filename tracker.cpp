c8265590c34be91d5bbeaad8d9912821be369d7e
Tuhin <hritabratabardhan13579@gmail.com>
2026-04-28 23:36:18 +0530

    Added file loader and full cli menu

--- tracker.cpp
+++ tracker.cpp
@@ -2,7 +2,19 @@
 #include<string>
 #include<fstream>
 
-using std::string;
+using std::string,std::cout,std::cin,std::endl;
+
+//defining all the functions
+void addProblem(string name,string tag);
+void ramLogChecker();
+void undoLast();
+void fileSaver();
+void addProblemHelperFunction();
+void welcomeMenu();
+void displayFromFile();
+void loadFromFile();
+void logCheckerHelperFunction();
+
 
 struct Node{
     string problem;
@@ -19,7 +31,7 @@ struct Node{
 
 Node* head = nullptr;
 Node* tail = nullptr;
-
+//Adding problems
 void addProblem(string name,string tag){
     Node* newNode = new Node(name,tag);
     if(head == NULL){
@@ -30,7 +42,9 @@ void addProblem(string name,string tag){
         head = newNode;
     }
 }
-void logChecker(){
+
+//prints the linked list
+void ramLogChecker(){
     if(head == NULL){
         std::cout<<"Log is empty"<<std::endl;
         return;
@@ -43,6 +57,7 @@ void logChecker(){
     std::cout<<"log ended"<<std::endl;
 }
 
+//deletes last node of linked list
 void undoLast(){
     if(head == NULL){
         std::cout<<"Log is empty.Enter something"<<std::endl;
@@ -61,34 +76,140 @@ void undoLast(){
     return;
 }
 
+//saves the files
 void fileSaver(){
     std::ofstream out("files.txt");
-    if(head == NULL){
-        out.close();
-        return;
-    }
-    if(head == tail){
-        out<<head->problem<<"-"<<head->difficulty<<std::endl;
-        out.close();
+    
+    if(!out){
+        cout<<"file not opened"<<endl;
         return;
     }
     Node* temp = head;
     while(temp != NULL){
-        out<<temp->problem<<"-"<<temp->difficulty<<std::endl;
+        out<<temp->problem<<"-"<<temp->difficulty<<endl;
         temp= temp->next;
     }
     out.close();
     return;
 }
+
+//helper functions for addproblem
+void addProblemHelperFunction(){
+    string problem,tag;
+    cout<<"Enter the Problem name"<<endl;
+    cin.ignore();
+    std::getline(cin,problem);
+    cout<<"Enter the difficulty"<<endl;
+    std::getline(cin,tag);
+
+    addProblem(problem,tag);
+    cout<<"problem added"<<endl;
+}
+
+
+//display menu
+void welcomeMenu(){
+    int choice;
+    do{
+        cout<<"welcome to Log Manager"<<endl;
+        cout<<"---Choose from the Menu---"<<endl;
+        cout<<"1.Add problem"<<endl;
+        cout<<"2.Delete last problem"<<endl;
+        cout<<"3.Check the log"<<endl;
+        cout<<"4.Save to File"<<endl;
+        cout<<"5.Return"<<endl;
+
+       
+        cin>>choice;
+
+        if(choice<1 || choice>5){
+            std::cerr<<"Error invalid input"<<endl;
+            cin.clear();
+            cin.ignore(1000,'\n');
+        }
+        
+        switch(choice){
+            case 1 :
+                addProblemHelperFunction();
+                break;
+            case 2:
+                undoLast();
+                cout<<"last checkout deleted"<<endl;
+                break;
+            case 3:
+                logCheckerHelperFunction();
+                break;
+            case 4:
+                fileSaver();
+                cout<<"file saved"<<endl;
+                break;
+            
+        }
+    }while(choice != 5);
+}
+
+//displays the content of the file
+void displayFromFile(){
+
+    std::ifstream in("files.txt");
+
+    
+    if (!in.is_open()) {
+        std::cerr << "Error: Could not open the file!" << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(in, line)) {
+        
+        std::cout << line << std::endl;
+    }
+
+    
+    in.close();
+}
+
+//loads the files contents and breaks them and adds to the linked list 
+void loadFromFile(){
+    std::ifstream in("files.txt");
+
+    if (!in.is_open()) {
+        std::cerr << "Error: Could not open the file!" << std::endl;
+        return;
+    }
+    string line;
+
+    while(std::getline(in,line)){
+        size_t pos = line.find("-");
+        if(pos != string::npos){
+            string pName = line.substr(0,pos);
+            string pDiff = line.substr(pos+1);
+            addProblem(pName,pDiff);
+        }
+    }
+    in.close();
+}
+
+//helper function for log checker
+void logCheckerHelperFunction(){
+    cout<<"dou You want to see the unsaved version or saved version(Not Applicable if already saved)"<<endl;
+    cout<<"1.saved version"<<endl;
+    cout<<"2.unsaved version"<<endl;
+    int n;
+    cin.ignore();
+    cin>> n;
+    if(n==1){
+        displayFromFile();
+    }else if(n==2){
+        ramLogChecker();
+    }else{
+        cout<<"enter a valid option"<<endl;
+    }
+}
+
 int main() {
-    addProblem("Two sum","Easy");
-    addProblem("Three sum","Medium");
-    addProblem("Merge k sorted List","Hard");
+    loadFromFile();
+    welcomeMenu();
 
-
-    undoLast();
-
-    logChecker();
-    fileSaver();
     return 0;
 }
